89fc6cea66f37e54d13a03a7eb8408f74ee0cc69
Rosa-Ramirez <rramireze5@miumg.edu.gt>
2022-03-29 21:07:54 -0600

    Se crea la funcion resta para la calculadora

--- CalculadoraEspecial-Java/src/calculadoraespecial/java/FuncionesBasicas.java
+++ CalculadoraEspecial-Java/src/calculadoraespecial/java/FuncionesBasicas.java
@@ -5,5 +5,7 @@ public class FuncionesBasicas {
     public double suma (double a, double b) {
         return a + b;
     }
-    
+    public double resta (double a, double b) {
+        return a - b;
+    }
 }
