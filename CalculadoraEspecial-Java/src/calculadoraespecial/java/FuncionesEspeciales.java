760309220ec48d2dc15798b7034adab895e5a065
Rosa-Ramirez <rramireze5@miumg.edu.gt>
2022-03-29 19:50:16 -0600

    Se agrega la funcion raiz para la calculadora

--- CalculadoraEspecial-Java/src/calculadoraespecial/java/FuncionesEspeciales.java
+++ CalculadoraEspecial-Java/src/calculadoraespecial/java/FuncionesEspeciales.java
@@ -4,5 +4,8 @@ public class FuncionesEspeciales {
     public double valorAbsoluto (double a) {
         return Math.abs(a);
     }
+    public double raiz (double a) {
+        return Math.sqrt(a);
+    }
     
 }
