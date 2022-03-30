98bda47dc303294a4adbc158ea565c8255b0459f
Rosa-Ramirez <rramireze5@miumg.edu.gt>
2022-03-29 19:46:22 -0600

    Se agrega la funcion valor absoluto para la calculadora

--- CalculadoraEspecial-Java/src/calculadoraespecial/java/FuncionesEspeciales.java
+++ CalculadoraEspecial-Java/src/calculadoraespecial/java/FuncionesEspeciales.java
@@ -1,13 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package calculadoraespecial.java;
 
-/**
- *
- * @author ramir
- */
 public class FuncionesEspeciales {
+    public double valorAbsoluto (double a) {
+        return Math.abs(a);
+    }
     
 }
